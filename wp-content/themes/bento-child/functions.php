6a8cc504c6f835cf49a7bd5e39d4fc1663c02d69
crazycodegirl <joannesullivan69@yahoo.com>
2017-06-26 09:54:01 -0600

    hopefully fixed google fonts error on line 8 functions .php

--- wp-content/themes/bento-child/functions.php
+++ wp-content/themes/bento-child/functions.php
@@ -5,7 +5,6 @@ function my_theme_enqueue_styles() {
   wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
   wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( $parent_style ), wp_get_theme()->get('Version'));
   wp_enqueue_style( 'font-awesome', get_template_directory_uri().'/includes/font-awesome/css/font-awesome.min.css', array(), null, 'all' );
-	wp_enqueue_style( 'google-fonts', bento_google_fonts(), array(), null );
 }
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 ?>
